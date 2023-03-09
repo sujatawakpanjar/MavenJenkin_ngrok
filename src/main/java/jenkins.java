39392aabcdded2f619d2a84260f0ca7f0eb59256
Sujata Gawai <sujatawakpanjar33@gmail.com>
2023-03-09 16:44:05 +0530

    first commit

--- src/main/java/jenkins.java
+++ src/main/java/jenkins.java
@@ -2,7 +2,7 @@
 public class jenkins {
 	
 	public static void main(String[] args) {
-		System.out.println("open jenkins!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("open jenkins!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 	} 
 	
 }
